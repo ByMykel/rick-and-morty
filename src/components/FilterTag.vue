932d523e6fe93cbacbe9c3750cb44758a52e7b7b
ByMykel <bymykel5@gmail.com>
2021-03-29 23:30:28 +0200

    Add transition component

--- src/components/FilterTag.vue
+++ src/components/FilterTag.vue
@@ -13,35 +13,44 @@
                 <Icons icon="chevron-down-dropdown" />
             </button>
         </div>
-        <div
-            class="z-50 origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-            v-show="show"
+        <transition
+            enter-active-class="transition ease-out duration-100"
+            leave-active-class="transition ease-in duration-75"
+            enter-class="transform opacity-0 -translate-y-3"
+            enter-to-class="transform opacity-100 translate-y-0"
+            leave-class="transform opacity-100 translate-y-0"
+            leave-to-class="transform opacity-0 -translate-y-3"
         >
-            <div class="py-1" role="none">
-                <div
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                    @click="
-                        (selected = ''),
-                            (show = false),
-                            $parent.$emit(event, selected)
-                    "
-                >
-                    All
-                </div>
-                <div
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 capitalize"
-                    v-for="(tag, index) in options"
-                    :key="index"
-                    @click="
-                        (selected = tag),
-                            (show = false),
-                            $parent.$emit(event, selected)
-                    "
-                >
-                    {{ tag }}
+            <div
+                class="z-50 origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                v-show="show"
+            >
+                <div class="py-1" role="none">
+                    <div
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        @click="
+                            (selected = ''),
+                                (show = false),
+                                $parent.$emit(event, selected)
+                        "
+                    >
+                        All
+                    </div>
+                    <div
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 capitalize"
+                        v-for="(tag, index) in options"
+                        :key="index"
+                        @click="
+                            (selected = tag),
+                                (show = false),
+                                $parent.$emit(event, selected)
+                        "
+                    >
+                        {{ tag }}
+                    </div>
                 </div>
             </div>
-        </div>
+        </transition>
     </div>
 </template>
 
