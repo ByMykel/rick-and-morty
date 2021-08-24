9459e9d151e83daba3d4d62798e8aa76d5ee0072
ByMykel <bymykel5@gmail.com>
2021-08-24 09:05:30 +0200

    feat: hide filters button until all the data is loaded.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,26 +1,27 @@
 <template>
     <div
-        class="mx-auto relative text-gray-600 w-full max-w-6xl sm:px-6 lg:px-8"
+        class="relative w-full max-w-6xl mx-auto text-gray-600 sm:px-6 lg:px-8"
     >
         <input
             type="text"
             v-model="searchText"
             placeholder="Search by name"
-            class="bg-white h-10 w-full px-10 pr-10 rounded-md text-sm focus:outline-none shadow-sm border border-gray-300 hover:shadow-md"
+            class="w-full h-10 px-10 pr-10 text-sm bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none hover:shadow-md"
             @keyup.prevent="$emit('fetch-data', searchText)"
         />
 
         <button
             type="submit"
-            class="absolute left-1 top-0 mt-1 mr-3 sm:left-7 lg:left-9 text-gray-700 hover:bg-gray-200 rounded-md p-1"
+            class="absolute top-0 p-1 mt-1 mr-3 text-gray-700 rounded-md left-1 sm:left-7 lg:left-9 hover:bg-gray-200"
             @click.prevent="$emit('fetch-data', searchText)"
         >
             <icons icon="search"></icons>
         </button>
 
         <button
+            v-if="!loading"
             type="submit"
-            class="absolute right-1 top-0 mt-1 sm:right-7 lg:right-9 text-gray-700 hover:bg-gray-200 rounded-md p-1"
+            class="absolute top-0 p-1 mt-1 text-gray-700 rounded-md right-1 sm:right-7 lg:right-9 hover:bg-gray-200"
             @click.prevent="$emit('toggle-filters')"
         >
             <icons icon="filter"></icons>
@@ -30,6 +31,8 @@
 
 <script>
 import Icons from "./Icons.vue";
+
+import { mapState } from "vuex";
 
 export default {
     name: "SearchInput",
@@ -42,6 +45,12 @@ export default {
         return {
             searchText: "",
         };
+    },
+
+    computed: {
+        ...mapState({
+            loading: (state) => state.characters.loading,
+        }),
     },
 };
 </script>
